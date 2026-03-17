9011c874776ab29a35d8af8086ec1b41d23c5b8b
literate-t <gaekoon@gmail.com>
2026-03-17 13:28:05 +0900

    feat: move() 함수의 방어 로직을 보완한다
    - totalRouteDistance 값이 정상적이지 않다면 globalProgressRatio = 1.0;을 추가해 클라이언트에서 렌더링이 일어나지 않도록 한다
    
    - 주행이 완료됐으면(if (totalRouteDistance <= accumulatedDistance)), globalProgressRatio = 1.0;을 추가한다

--- src/main/java/dashboard/state/VehicleInfo.java
+++ src/main/java/dashboard/state/VehicleInfo.java
@@ -6,17 +6,21 @@ import java.util.List;
 public class VehicleInfo {
 
   private final String vehicleId;
-  private final double speed;
+  private final double baseSpeed;
   private List<RoadEdge> plannedPath;
+
+  private double globalProgressRatio = 0.0;
+
+  private double accumulatedDistance;
   private double totalRouteDistance;
-  private double currentAccumulatedDistance;
+
   private boolean finished;
   private boolean routeUpdated;
 
-  public VehicleInfo(String vehicleId, List<RoadEdge> plannedPath, double speed) {
+  public VehicleInfo(String vehicleId, List<RoadEdge> plannedPath, double baseSpeed) {
     this.vehicleId = vehicleId;
-    this.speed = speed;
-    this.currentAccumulatedDistance = 0.0;
+    this.baseSpeed = baseSpeed;
+    this.accumulatedDistance = 0.0;
 
     updatePath(plannedPath);
   }
@@ -29,18 +33,31 @@ public class VehicleInfo {
 
   // 한 틱마다 호출
   public void move(double deltaSeconds) {
-    if (finished) {
+    if (finished || plannedPath.isEmpty()) {
       return;
     }
 
     // 틱 단위로 이동하는 물리적 거리
-    double moveDistance = speed * deltaSeconds;
-    currentAccumulatedDistance += moveDistance;
+    double moveDistance = baseSpeed * deltaSeconds;
+    accumulatedDistance += moveDistance;
 
-    if (currentAccumulatedDistance >= totalRouteDistance) {
-      currentAccumulatedDistance = totalRouteDistance;
+    if (totalRouteDistance <= accumulatedDistance) {
       finished = true;
+      globalProgressRatio = 1.0;
+      accumulatedDistance = totalRouteDistance;
+      return;
     }
+
+    updateGlobalProgressRatio();
+  }
+
+  private void updateGlobalProgressRatio() {
+    if (totalRouteDistance <= 0) {
+      globalProgressRatio = 1.0;
+      return;
+    }
+
+    globalProgressRatio = Math.min(1, accumulatedDistance / totalRouteDistance);
   }
 
   // 현재 간선의 진행률
@@ -49,7 +66,7 @@ public class VehicleInfo {
       return 1;
     }
 
-    return currentAccumulatedDistance / totalRouteDistance;
+    return globalProgressRatio;
   }
 
   public boolean isFinished() {
